918bf525295817e4fc406b1c6e71fe87c5d7fa24
ymz <2858392644@qq.com>
2019-12-13 20:41:06 +0800

    2.0

--- src/main/java/com/shsxt/Test04.java
+++ src/main/java/com/shsxt/Test04.java
@@ -3,7 +3,7 @@ package com.shsxt;
 public class Test04 {
     public static void main(String[] args) {
         System.out.println("test04...");
-        System.out.println("dev_2.0--->test04...。");
+        System.out.println("dev_2.0--->test04...");
         System.out.println("dev_2.0--->test04...第二次文件合并....");
     }
 }
